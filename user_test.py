b792b3fac256fdca8051e3891d179b3a29bcd71e
Paul Munyao <paulmunyao094@gmail.com>
2022-02-19 18:51:01 +0300

    Edit variable  of new user

--- user_test.py
+++ user_test.py
@@ -14,9 +14,12 @@ def setUp(self):
     '''
     Set up method to run before each test case
     '''
-    self.user = User("Paul", "Munyao", "!@123")
+    self.new_user = User("Paul", "Munyao", "!@123")
+
 
 def test__init(self):
     '''
     test__init__ test case to test if the object is initialized properly
     '''
+
+    self.assertEqual (self.new_user first_name "Paul")
