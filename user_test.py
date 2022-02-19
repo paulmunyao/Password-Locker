d7e927f7230c52a1b21d58c74f2edcacd4cfc619
Paul Munyao <paulmunyao094@gmail.com>
2022-02-19 18:19:55 +0300

    Add description inside the args

--- user_test.py
+++ user_test.py
@@ -5,4 +5,6 @@ from user import User
 class UserTest(unittest.TestCase):
     '''
     Test that defines test cases for the User class
+    Args:
+        unitest.Testcase: Testcase that helps in creating test cases for class User.
     '''
