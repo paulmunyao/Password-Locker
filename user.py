eea9b00a598bbbfe3874438580d3bf8adb8d3fd3
Paul Munyao <paulmunyao094@gmail.com>
2022-02-20 11:12:34 +0300

    Add doc strings

--- user.py
+++ user.py
@@ -15,7 +15,10 @@ def __init__(self, first_name, last_name, password):
          passord : password of the user
     '''
 
-         def save_user(self):
+    def save_user(self):
+             '''
+
+             '''
 
     self.first_name = first_name
     self.last_name = last_name
