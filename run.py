4abf563b964c255bd44756a1c145f1380c36f552
Paul Munyao <paulmunyao094@gmail.com>
2022-02-20 15:15:44 +0300

    Remove unnecesary code line

--- run.py
+++ run.py
@@ -5,24 +5,3 @@ def create_user(username, password):
     function to create a new user
     '''
 
-# username = input("Enter user name: ")
-# password = input("Enter password: ")
-# confirm_username = input("Enter user name: ")
-# confirm_password = input("Enter password: ")
-
-
-# if username == confirm_username:
-#     print("Welcome.")
-
-# else:
-#      print("Username doesn't match")
-
-
-# if password == confirm_password:
-#         print("Welcome.")
-
-# else:
-#         print("Password doesn't match")
-# user = getpass.getpass()
-# while True:
-#     password = getpass.getpass("")
