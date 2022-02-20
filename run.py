1f084a1cec601b8622942ac05d4cc40b28974e9c
Paul Munyao <paulmunyao094@gmail.com>
2022-02-20 13:19:02 +0300

    Edit username

--- run.py
+++ run.py
@@ -4,3 +4,6 @@ firstname = input("Enter first name: ")
 lastname = input("Enter last name: ")
 password = input("Enter password: ")
 
+user = getpass.getpass()
+while True:
+    password = getpass.getpass("")
--- user.py
+++ user.py
@@ -5,7 +5,7 @@ class User:
 
 user_list = []
 
-def __init__(self, first_name, last_name, password):
+def __init__(self, user_name, password):
     self.first_name = first_name
     self.last_name = last_name
     self.password = password
