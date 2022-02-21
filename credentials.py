0e6d172dcd463efaa5c224a7473730656716a81b
Paul Munyao <paulmunyao094@gmail.com>
2022-02-21 08:31:43 +0300

    Initializing class credntilas

--- credentials.py
+++ credentials.py
@@ -4,3 +4,14 @@ class credentials:
     '''
 
     credentials_list = []
+
+    def __init__(self, user_name, password):
+
+        self.user_name = user_name
+        self.password = password
+        '''
+            __init__method that helps define properties for our objects.
+            Args:
+                user_name: user name
+                passord : password of the user
+            '''
