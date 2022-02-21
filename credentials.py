4244bba0f65921a007719e32b767945809c7c257
Paul Munyao <paulmunyao094@gmail.com>
2022-02-21 08:35:53 +0300

    Method to save credentials

--- credentials.py
+++ credentials.py
@@ -15,3 +15,10 @@ class credentials:
                 user_name: user name
                 passord : password of the user
             '''
+
+    def save_credentials(self):
+        '''
+        save_credentials method saves the user object into the database/user_list
+        '''
+
+        credentials.user_list.append(self)
