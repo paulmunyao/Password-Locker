4b7b4b6e4b3bd974a3d863a6836996f58f4c9be6
Paul Munyao <paulmunyao094@gmail.com>
2022-02-21 05:49:04 +0300

    Test to see if the user can delete a user

--- user_test.py
+++ user_test.py
@@ -51,7 +51,10 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user()
         test_user = User("Test","user")
-        self.assertEqual(User.user_list.remove(self))
+        test_user.save_user()
+
+        self.new_user.delete_user()
+        self.assertEqual(len(User.user_list), 1)
 
 
 if __name__ == "__main__":
